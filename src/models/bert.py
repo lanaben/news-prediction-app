2e043c5888c769893b1fae39d25d5bbdc835758d
Lana Benedicic <lana.benedicic@gmail.com>
2024-05-31 19:21:23 +0200

    Pip pipeline.

--- src/models/bert.py
+++ src/models/bert.py
@@ -2,16 +2,16 @@ import pandas as pd
 from keybert import KeyBERT
 import os
 
-raw_data_path = "../../data/raw/articles.csv"
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(ROOT_DIR, '..', '..', 'data')
+raw_data_path = os.path.join(DATA_DIR, "raw", "articles.csv")
+processed_data_path = os.path.join(DATA_DIR, "processed", "articles_with_keywords.csv")
+
 raw_df = pd.read_csv(raw_data_path)
 
 kw_model = KeyBERT()
 
 raw_df['Keywords'] = raw_df['Title'].apply(lambda x: ", ".join([keyword[0] for keyword in kw_model.extract_keywords(x)]))
-
-processed_data_dir = "../../data/processed"
-os.makedirs(processed_data_dir, exist_ok=True)
-processed_data_path = os.path.join(processed_data_dir, "articles_with_keywords.csv")
 
 raw_df.to_csv(processed_data_path, index=False)
 
