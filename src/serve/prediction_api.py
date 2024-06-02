e28c1f46b91feec6c7dc282c6a045e11ecd11bf0
Lana Benedicic <lana.benedicic@gmail.com>
2024-06-02 21:25:08 +0200

    Pipeline fix.

--- src/serve/prediction_api.py
+++ src/serve/prediction_api.py
@@ -1,13 +1,70 @@
 import pandas as pd
 from flask import Flask, request, jsonify
-import joblib
-from flask_cors import CORS
+import mlflow.sklearn
+from mlflow.tracking import MlflowClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+
+mlflow_tracking_uri = os.getenv("MLFLOW_URL")
+
+if not mlflow_tracking_uri:
+    raise ValueError("MLFLOW_URL environment variable not set. Please check your .env file.")
+
+mlflow.set_tracking_uri(mlflow_tracking_uri)
+
+# Set the experiment name where the model is stored
+experiment_name = "news_prediction_model_metrics"
+
+# Load the model from MLflow registry
+model_name = "news_prediction_model"
+
+def load_model():
+    mlflow.set_tracking_uri(mlflow_tracking_uri)
+    mlflow.set_experiment(experiment_name)
+    model_uri = f"models:/{model_name}/Production"
+    loaded_model = mlflow.sklearn.load_model(model_uri)
+    return loaded_model
 
 # Load the model
-model = joblib.load('../../models/news_prediction_model.h5')
+model = load_model()
+
+#TODO: endpoint for models, experiments, metrics
+
+# def print_models_info(mv):
+#     for m in mv:
+#         print(f"name: {m.name}")
+#         print(f"latest version: {m.version}")
+#         print(f"run_id: {m.run_id}")
+#         print(f"current_stage: {m.current_stage}")
+#
+# # Get all model versions and their corresponding experiments
+# client = MlflowClient()
+# models = client.get_latest_versions(model_name, stages=None)
+# print_models_info(models)
+# print("--")
+#
+# all_experiments = client.search_experiments()
+# print(all_experiments)
+#
+# all_runs = mlflow.search_runs(search_all_experiments=True)
+# import pandas as pd
+#
+# # Set display option to show all columns
+# pd.set_option('display.max_columns', None)
+#
+# # Now print the DataFrame
+# print(all_runs)
+#
+# try:
+#     # Save DataFrame to a CSV file
+#     all_runs.to_csv('all_runs.csv', index=False)
+#     print("File saved successfully.")
+# except Exception as e:
+#     print(f"An error occurred while saving the file: {e}")
 
 @app.route('/predict', methods=['POST'])
 def predict():
