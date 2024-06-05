9f9606bd8a727c2b61cda97f0943dbf73e9bc4d7
Lana Benedicic <lana.benedicic@gmail.com>
2024-06-05 17:28:50 +0200

    Vue client, model fix.

---
+++ src/client/news-prediction-app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+#app {
+  font-family: 'Roboto', sans-serif;
+  text-align: center;
+}
+</style>
